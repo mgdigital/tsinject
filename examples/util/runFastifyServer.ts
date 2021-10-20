24dea15f08ac3eb27186c541d5b6dbcdefc628ed
mgdigital <michael@mgdigital.co.uk>
2021-10-20 08:00:40 +0100

    Improve log message

--- examples/util/runFastifyServer.ts
+++ examples/util/runFastifyServer.ts
@@ -8,7 +8,7 @@ const runFastifyServer = (
 ): ServerRunner =>
   async port =>
     fastify.listen(port)
-      .then(address => logger.info(`Server listening at ${address}`))
+      .then(address => logger.info(`Server listening at ${address} with routes:\n\n${fastify.printRoutes()}`))
       .catch((error: Error) => {
         logger.error(`Error starting server: ${error.message}`, { error })
         process.exit(1)
